3c550c5dd8237728f1fe68326cf1de94da9bf6c3
Adam <AGCapdeville@gmail.com>
2024-09-12 21:02:40 -0700

    Fixed Map Maker
    Rows & Cols, reverse and un-reverse for save & load, due to Unity

--- func.js
+++ func.js
@@ -174,28 +174,30 @@ function saveMapToJSON() {
 
     const validSpaces = ["space", "dirt", "grass", "stone", "river"];
     const walls = ["stone-wall"];
+    
+    // since Z is depth, we use x 
     const cardinal_directions = {
-        "north":{x: 0, z: 1},
-        "east":{x: 1, z: 0}, 
-        "south":{x: 0, z: -1}, 
-        "west":{x: -1, z: 0}
+        "north":{x: 1, z: 0},
+        "east":{x: 0, z: 1}, 
+        "south":{x: -1, z: 0}, 
+        "west":{x: 0, z: -1}
     };
 
     Object.entries(tileObjectDictionary).forEach(([key, value]) => {
 
         if (validSpaces.includes(value.terrain)) {
-            
-            let current_x = Number(key.split(',')[0]);
-            let current_z = Number(key.split(',')[2]);
+
+            let current_x = parseInt(key.split(',')[0]);
+            let current_z = parseInt(key.split(',')[2]);
 
             Object.entries(cardinal_directions).forEach(([k, v]) => {
 
                 let check_position = (current_x + v.x) + ",0," + (current_z + v.z);
 
                 if (walls.includes(tileObjectDictionary[check_position].terrain)) {
-                    tileObjectDictionary[key][k] = "stone-wall";
+                    tileObjectDictionary[key.split(',')[0] + ",0," + key.split(',')[2]][k] = "stone-wall";
                 } else {
-                    tileObjectDictionary[key][k] = "no-wall";
+                    tileObjectDictionary[key.split(',')[0] + ",0," + key.split(',')[2]][k] = "no-wall";
                 }
             });
         }
@@ -204,12 +206,14 @@ function saveMapToJSON() {
     let onlySpacesDictionary = {"map":{}};
     Object.entries(tileObjectDictionary).forEach(([key, value]) => {
         if (validSpaces.includes(value.terrain)) {
-            onlySpacesDictionary.map[(key.split(',')[0]/2 - 0.5) + ',0,' + (key.split(',')[2]/2 - 0.5)] = value;
+            // Since Z in unity is rows and X is Columns
+            onlySpacesDictionary.map[(key.split(',')[2]/2 - 0.5) + ',0,' + (key.split(',')[0]/2 - 0.5)] = value;
         }
     });
 
-    onlySpacesDictionary["rows"] = localStorage.getItem("rows");
-    onlySpacesDictionary["cols"] = localStorage.getItem("cols");
+    // Since Z in unity is rows and X is Columns
+    onlySpacesDictionary["rows"] = localStorage.getItem("cols");
+    onlySpacesDictionary["cols"] = localStorage.getItem("rows");
 
     // Convert the JSON object to a string and then to a Blob
     let jsonString = JSON.stringify(onlySpacesDictionary, null, 2);
@@ -259,10 +263,10 @@ function loadJSONMap(inputElement) {
 }
 
 function handleLoadedJSON(JSON) {
-    changeMapTo(JSON.rows, JSON.cols);
+    changeMapTo(parseInt(JSON.cols), parseInt(JSON.rows));
     
-    for (let z = JSON.rows; z < -1; z++) {
-        for (let x = 0; x < JSON.cols; x++) {
+    for (let z = 0; z < parseInt(JSON.cols); z++) {
+        for (let x = 0; x < parseInt(JSON.rows); x++) {
             shiftedZ = 2 * z + 1;
             shiftedX = 2 * x + 1;
 
